211e03c63ad62ff1d686e43099a51f749fbf21be
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-02 11:17:19 +0530

    feat: creating a new travel destination
    Creating a function createTravelDestination that accepts an object containing travel destination data and adds a new destination to the database.

--- index.js
+++ index.js
@@ -2,3 +2,26 @@ const mongoose = require("mongoose")
 require("./db")
 const User = require("./models/user")
 const Destination = require("./models/destination")
+
+const sampleDestination = {
+    name: "Sample Destination",
+    location: {
+        city: "Sample City",
+        country: "Sample Country"
+    },
+    description: "This is a sample travel destination with a brief description.",
+    rating: 8
+};
+
+//creating new travel destinaton
+async function createNewTravelDestination(travelDestination) {
+    try {
+        const destination = new Destination(travelDestination);
+        const saveDestinaton = await destination.save()
+        console.log("New destination added successfully ", saveDestinaton)
+
+    } catch (error) {
+        console.log("Failed to add new destination ", error)
+    }
+}
+createNewTravelDestination(sampleDestination)
